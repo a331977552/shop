6011e2b6f7d303650c0c6469909995c32ffa987d
cody <331977552@qq.com>
2021-06-30 00:17:39 +0800

    add docs

--- admin-client/src/main/resources/webapp/admin/src/pages/app/AppFooter.tsx
+++ admin-client/src/main/resources/webapp/admin/src/pages/app/AppFooter.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function AppFooter() {
     return (
-        <div style={{textAlign: 'center', padding:'0 10px 10px 10px'}}>Ant Design ©2018 Created by Ant UED</div>
+        <div style={{textAlign: 'center', padding:'0 10px 10px 10px'}}>Designed by Cody, feel free to use :), email:
+            <a style={{marginLeft:'10px'}} href="mailto:a13521874221@gmail.com">a13521874221@gmail.com</a>
+        </div>
     );
 }
 
